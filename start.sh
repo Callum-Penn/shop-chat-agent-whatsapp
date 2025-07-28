9e80c0c50a0ae4b499920b77721ae8da00a50044
Callum-Penn <callum@penn.co.uk>
2025-07-28 09:20:52 +0100

    Force fresh deployment with host binding fix - version 1.0.2

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ npx prisma generate
 # Push database schema (create tables)
 npx prisma db push
 
-# Start the app
+# Start the app - bind to all interfaces for external access
 remix-serve ./build/server/index.js --host 0.0.0.0 
